01475a922a5ae28fd6f7279c8a69a32bdc503628
Arjun Saili <arjun7.saili@gmail.com>
2022-01-08 05:57:34 -0500

    Create a Basic Version of API Data Getter Module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+const apiLogic = (() =>{
+
+    async function queryAPI(query){
+        try{
+            const apiResponse = await fetch(`https://api.nasa.gov/planetary/apod?api_key=RniEDW1yakFezEGAKtaMcLMz2KtS5xUKPdMUlky0&date=${query}`)
+            const apiResponseJSON = await apiResponse.json();
+            console.log(apiResponseJSON)
+        }
+        catch{
+            console.log("error")
+        }
+    }
+
+    return {queryAPI}
+})()
+
+apiLogic.queryAPI("2002-10-02");
