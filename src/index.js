cc888aa519234d0847912c7376b033e61a0d161c
Arjun Saili <arjun7.saili@gmail.com>
2022-01-08 09:05:53 -0500

    Linted Code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import "./styles.css";
 
-const apiLogic = (() =>{
-
-    async function queryAPI(query){
-        try{
-            const apiResponse = await fetch(`https://api.nasa.gov/planetary/apod?api_key=RniEDW1yakFezEGAKtaMcLMz2KtS5xUKPdMUlky0&date=${query}`)
-            const apiResponseJSON = await apiResponse.json();
-            return(apiResponseJSON)
-        }
-        catch{
-            // Error Handling is yet to be implemented
-            console.log("yikes")
-        }
+const apiLogic = (() => {
+  async function queryAPI(query) {
+    try {
+      const apiResponse = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=RniEDW1yakFezEGAKtaMcLMz2KtS5xUKPdMUlky0&date=${query}`
+      );
+      const apiResponseJSON = await apiResponse.json();
+      return apiResponseJSON;
+    } catch {
+      // Error Handling is yet to be implemented
+      console.log("yikes");
     }
+  }
 
-    function getRandomDate() {
-        const todayDate = new Date();
-        const startDate = new Date(1995, 5, 16);
-        return (new Date(startDate.getTime() + Math.random() * (todayDate.getTime() - startDate.getTime())).toLocaleDateString('fr-CA'));
+  function getRandomDate() {
+    const todayDate = new Date();
+    const startDate = new Date(1995, 5, 16);
+    return new Date(
+      startDate.getTime() +
+        Math.random() * (todayDate.getTime() - startDate.getTime()),
+    ).toLocaleDateString("fr-CA");
+  }
 
-    }
-
-    return {queryAPI, getRandomDate}
+  return { queryAPI, getRandomDate };
 })();
