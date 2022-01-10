25403b4d9a1458b43bd3f5f07eb40bbf158aece4
Arjun Saili <arjun7.saili@gmail.com>
2022-01-09 20:20:58 -0500

    Temporarily Set Title of Post & Hdurl/url of image

--- src/displayControl.js
+++ src/displayControl.js
@@ -3,7 +3,15 @@ import { apiLogic } from ".";
 export const displayControl = (()=>{
     async function tempSetImg() {
         const temp = await apiLogic.getRandomAPOD()
-        document.querySelector("#test-img").src = temp["hdurl"];
+        console.log(temp)
+        if(temp["hdurl"]){
+            document.querySelector("#test-img").src = temp["hdurl"];
+        }
+        else{
+            document.querySelector("#test-img").src = temp["url"];
+        }
+        document.querySelector("#test-name").textContent = temp["title"];
+        // 
     }
 
     return {tempSetImg}
