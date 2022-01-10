0f183d9955fc0d0ee70f4c778022bb596780468c
Arjun Saili <arjun7.saili@gmail.com>
2022-01-09 20:15:39 -0500

    Set API Fetch Mode to CORS

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ export const apiLogic = (() => {
   async function queryAPI(query) {
     try {
       const apiResponse = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=RniEDW1yakFezEGAKtaMcLMz2KtS5xUKPdMUlky0&date=${query}`
+        `https://api.nasa.gov/planetary/apod?api_key=RniEDW1yakFezEGAKtaMcLMz2KtS5xUKPdMUlky0&date=${query}`,
+        {mode: 'cors'}
       );
       const apiResponseJSON = await apiResponse.json();
       return apiResponseJSON;
