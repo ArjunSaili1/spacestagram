853a927c385448b2d2bde3293702c84faee58683
Arjun Saili <arjun7.saili@gmail.com>
2022-01-10 01:43:19 -0500

    Implement Description, See More Button Dynamically

--- src/displayControl.js
+++ src/displayControl.js
@@ -9,7 +9,7 @@ export const displayControl = (()=>{
         cardContainer.classList.add("feed-card");
         const header = await createCardHeader(apodData);
         const mediaContainer = await createCardMediaContainer(apodData);
-        const footer = createCardFooter();
+        const footer = await createCardFooter(apodData);
         cardContainer.appendChild(header);
         cardContainer.appendChild(mediaContainer);
         cardContainer.appendChild(footer);
@@ -31,7 +31,7 @@ export const displayControl = (()=>{
     }
 
     async function createCardMediaContainer(apodData){
-        const mediaContainer = document.createElement("div");
+        const mediaContainer = document.createElement("figure");
         mediaContainer.classList.add("media-container");
         if(apodData["media_type"] == "video"){
             const video = document.createElement("iframe");
@@ -58,12 +58,35 @@ export const displayControl = (()=>{
         return mediaContainer;
     }
     
-    function createCardFooter(){
+    async function createCardFooter(apodData){
         const footer = document.createElement('footer');
         footer.classList.add("post-footer");
         const likeContainer = createLikeContainer();
         footer.appendChild(likeContainer);
+        const description = await createCardDescription(apodData);
+        footer.appendChild(description);
         return footer;
+    }
+
+    async function createCardDescription(apodData){
+        const descriptionContainer = document.createElement("div");
+        descriptionContainer.classList.add("description-container");
+        const description = document.createElement("figcaption");
+        description.classList.add("description");
+        if(apodData["explanation"].length > 500){
+            description.textContent = apodData["explanation"].slice(0,500);
+            const seeMoreBtn = document.createElement("button");
+            const seeMoreTxt = document.createElement("h5");
+            seeMoreTxt.textContent = "...see more";
+            seeMoreBtn.appendChild(seeMoreTxt);
+            seeMoreBtn.classList.add("see-more-btn");
+            seeMoreBtn.addEventListener("click", ()=>{
+                description.textContent = apodData["explanation"];
+            })
+            description.appendChild(seeMoreBtn);
+        }
+        descriptionContainer.appendChild(description);
+        return descriptionContainer;
     }
 
     function createLikeContainer(){
